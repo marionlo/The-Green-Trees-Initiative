4b7f853831b6cbf67c2513a6febd7877a7d6bcd1
Marion Lotito <marion.lotito@gmail.com>
2021-08-12 20:53:50 +0200

    create hero banner mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,28 @@ module.exports = {
     extend: {
       fontFamily: {
         cormorant: ["Cormorant Garamond", "serif"],
+        flood: ["Flood Std Regular", "sans-serif"],
       },
       colors: {
         deepblue: "#354463",
         lime: "#C1F7D5",
+      },
+      backgroundImage: (theme) => ({
+        "hero-home-mobile":
+          "url('/assets/img/annie-spratt-135307-unsplash.png')",
+      }),
+      fontSize: {
+        "1xl": "1.375rem",
+        "4-5xl": "2.75rem",
+      },
+      height: {
+        herohome: "37rem",
+      },
+      margin: {
+        18: "4.5rem",
+      },
+      width: {
+        148: "19.5rem",
       },
     },
   },
