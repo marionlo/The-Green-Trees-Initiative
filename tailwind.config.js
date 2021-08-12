0c0a89120f1bb36e29c3b9ad579020d58b9fa9c1
Marion Lotito <marion.lotito@gmail.com>
2021-08-12 16:38:41 +0200

    create the nav mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 module.exports = {
-    purge: [],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {},
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        cormorant: ["Cormorant Garamond", "serif"],
+      },
+      colors: {
+        deepblue: "#354463",
+        lime: "#C1F7D5",
+      },
     },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
